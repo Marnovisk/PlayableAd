8e74e06cb6539c954bad976f23b0fc1777ff6c57
Denis Dias <mdddenis@gmail.com>
2026-01-26 18:39:47 -0300

    Item rotation and button fix
    The previous rotation was interfering with the button click, some ajustsments were made and know the button serves also as a sprite for the item object

--- Assets/SRC/Scripts/ItemScript.cs
+++ Assets/SRC/Scripts/ItemScript.cs
@@ -5,24 +5,48 @@ using UnityEngine.UI;
 public class ItemScript : MonoBehaviour
 {
     private ItemScriptable _brain;
-    private Image _image;
+    [SerializeField]  private Image _image;
+    [SerializeField]  private GameObject _imageObject;
+    [SerializeField]  private RectTransform _imageRect;
     private RectTransform _rectTransform;
     private GameplayScript _gmScript;
     public void Init(ItemScriptable brain)
     {
         _brain = brain;
         _rectTransform = GetComponent<RectTransform>();
-        SetSprite();
+        GetImage();        
         SetPosition();
-        //SetRotation();
+    }
+
+    private void GetImage()
+    {
+        if (!_brain) return;
+
+        //Image[] _images = GetComponentsInChildren<Image>();
+        //foreach (Image image in _images)
+        //{
+        //    if(!image.GetComponent<Button>())
+        //    {
+        //        _image = image;
+        //    }
+        //}
+
+
+        _image = GetComponentInChildren<Image>();
+        _imageObject = _image.gameObject;
+        _imageRect = _imageObject.GetComponent<RectTransform>();
+
+        SetSprite();
+        SetRotation();
     }
 
     private void SetSprite()
     {
         if(!_brain) return;
+        if(!_image) return;
 
-        _image = GetComponent<Image>();
         _image.sprite = _brain.Image;
+        Debug.Log("Sprite Setted");
     }
 
     private void SetPosition()
@@ -38,11 +62,10 @@ public class ItemScript : MonoBehaviour
     private void SetRotation()
     {
         if (!_brain) return;
-        float randomX = Random.Range(-200f, 550f);
-        float randomY = Random.Range(-400, 400);
-        float randomZ = Random.Range(0, 10);
+        if(!_imageObject) return;
+        float randomZ = Random.Range(0, 380);
 
-        _rectTransform.rotation = new Quaternion(randomX, randomY, randomZ, 0);
+        _imageRect.rotation = Quaternion.Euler(0, 0, randomZ);
     }
 
     public void SlectItem()
@@ -51,6 +74,7 @@ public class ItemScript : MonoBehaviour
         Debug.Log("Click");
         if (_gmScript == null) return;
         _gmScript.AddItenOnBar(this.gameObject);
+        _imageRect.rotation = Quaternion.Euler(0, 0, 90);
     }
     
 }
