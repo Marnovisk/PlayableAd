e193ee9d0f52fe7b7d46c853cfa5a39464dc8e92
Denis Dias <mdddenis@gmail.com>
2026-01-28 18:54:57 -0300

    Win and Lose Screen
    Game over screen now displays win or lose assets based on the condition that it receives

--- Assets/SRC/Scripts/GameManagerScript.cs
+++ Assets/SRC/Scripts/GameManagerScript.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using static UnityEditor.Progress;
 
 public class GameManagerScript : MonoBehaviour
@@ -13,9 +15,27 @@ public class GameManagerScript : MonoBehaviour
     public int _itensQTD;
     private ItemScript _itemScript;
     private float _time;
+    private bool _done = false;
+    public GameObject _infoBar;
+    public GameObject _targetPrefab;
+    public GameObject _collectBar;
+    public GameObject _gameOverScreen;
+    public GameObject _gameOverheart;
+    public GameObject _gameOverStars;
+    public GameObject _gameOverBG;
+    public GameObject _gameOverBTN;
+    public Sprite _stars;
+    public Sprite _heart;
+    public Sprite _winBTN;
+    public Sprite _loseBTN;
+    public Sprite _winBG;
+    public Sprite _loseBG;
+
+
 
     private void Start()
     {
+        _gameOverScreen.SetActive(false);
         _time = 30.0f;
         foreach (ItemScriptable item in _brains)
         {
@@ -39,14 +59,17 @@ public class GameManagerScript : MonoBehaviour
 
     private void UpdateTimer()
     {
-        if (_time >= 0)
+        if (!_done)
         {
-            _time -= Time.deltaTime;
-            _timeText.SetText("0:" + Mathf.CeilToInt(_time).ToString());
-        }
-        else
-        {
-            GameOver();
+            if (_time >= 0)
+            {
+                _time -= Time.deltaTime;
+                _timeText.SetText("0:" + Mathf.CeilToInt(_time).ToString());
+            }
+            else
+            {
+                GameOver();
+            }
         }
     }
 
@@ -54,12 +77,40 @@ public class GameManagerScript : MonoBehaviour
     {
         if (type1 == 0 && type2 == 0 && type3 == 0)
         {
+            _done = true;
+            GameOverScreenCall(true);
             Debug.Log("Win");
         }
     }
 
     void GameOver()
     {
+        _done = true;
+        GameOverScreenCall(false);
         Debug.Log("Lose");
+    }
+
+    void GameOverScreenCall(bool win)
+    {
+        _gameOverScreen.SetActive(true);
+        _infoBar.SetActive(false);
+        _targetPrefab.SetActive(false);
+        _collectBar.SetActive(false);
+
+        if(win)
+        {
+            _gameOverBG.GetComponent<Image>().sprite = _winBG;
+            _gameOverheart.SetActive(false);
+            _gameOverStars.SetActive(true);
+            _gameOverBTN.GetComponent<Image>().sprite = _winBTN;
+        }
+        else
+        {
+            _gameOverBG.GetComponent<Image>().sprite = _loseBG;
+            _gameOverheart.SetActive(true);
+            _gameOverStars.SetActive(false);
+            _gameOverBTN.GetComponent<Image>().sprite = _loseBTN;
+            _gameOverBTN.transform.GetChild(0).gameObject.SetActive(false);
+        }
     }
 } 
