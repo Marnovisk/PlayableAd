e35379cc6bc5f2e5ebf2071b1df4a4ce24ecfc14
Denis Dias <mdddenis@gmail.com>
2026-01-28 02:42:44 -0300

    Timer
    Timer is now functional

--- Assets/SRC/Scripts/GameManagerScript.cs
+++ Assets/SRC/Scripts/GameManagerScript.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
@@ -7,10 +8,13 @@ public class GameManagerScript : MonoBehaviour
     public GameObject _itemPrefab;
     public GameObject _gmCanva;
     public TargetBarScript _barScript;
+    public TMP_Text _timeText;
     private ItemScript _itemScript;
+    private float _time;
 
     private void Start()
     {
+        _time = 30.0f;
         foreach (ItemScriptable item in _brains)
         {
             int value = Random.Range(20, 40);
@@ -24,6 +28,29 @@ public class GameManagerScript : MonoBehaviour
             _barScript.SetTargetItem(item.Image, value);
 
         }
+
+    }
+
+    private void Update()
+    {
+        UpdateTimer();
+    }
+
+    private void UpdateTimer()
+    {
+        if (_time >= 0)
+        {
+            _time -= Time.deltaTime;
+            _timeText.SetText("0:" + Mathf.CeilToInt(_time).ToString());
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
 
     }
 } 
